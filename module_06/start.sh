3e8d52b85a5b33031297bae4fb54c1872afc5fe1
nnshalaeva <130309175+nnshalaeva@users.noreply.github.com>
2023-05-28 21:58:30 +0300

    Модуль 7
    Модуль 7

--- module_06/start.sh
+++ module_06/start.sh
@@ -1,18 +1,23 @@
-echo "without cache"
-echo "время 60 количество потоков 1 количество соединений 1"
-wrk -d 60 -t 1 -c 1 --latency -s ./get.lua http://localhost:8081/
-echo "время 60 количество потоков 10 количество соединений 10"
-wrk -d 60 -t 10 -c 10 --latency -s ./get.lua http://localhost:8081/
-echo "время 60 количество потоков 50 количество соединений 50"
-wrk -d 60 -t 50 -c 50 --latency -s ./get.lua http://localhost:8081/
+echo "without cache" >> cache_load_result.md
+echo "время 60 количество потоков 1 количество соединений 1"  >> cache_load_result.md
+wrk -d 60 -t 1 -c 1 --latency -s ./get.lua http://localhost:8081/  >> cache_load_result.md
+echo " " >> cache_load_result.md
+echo "время 60 количество потоков 10 количество соединений 10"  >> cache_load_result.md
+wrk -d 60 -t 10 -c 10 --latency -s ./get.lua http://localhost:8081/  >> cache_load_result.md
+echo " " >> cache_load_result.md
+echo "время 60 количество потоков 50 количество соединений 50"  >> cache_load_result.md
+wrk -d 60 -t 50 -c 50 --latency -s ./get.lua http://localhost:8081/  >> cache_load_result.md
+echo " " >> cache_load_result.md
 
 
-echo "with cache"
-echo "время 60 количество потоков 1 количество соединений 1"
-wrk -d 60 -t 1 -c 1 --latency -s ./get.lua http://localhost:8082/
-echo "время 60 количество потоков 10 количество соединений 10"
-wrk -d 60 -t 10 -c 10 --latency -s ./get.lua http://localhost:8082/
-echo "время 60 количество потоков 50 количество соединений 50"
-wrk -d 60 -t 50 -c 50 --latency -s ./get.lua http://localhost:8082/
+echo "with cache"  >> cache_load_result.md
+echo "время 60 количество потоков 1 количество соединений 1"  >> cache_load_result.md
+wrk -d 60 -t 1 -c 1 --latency -s ./get.lua http://localhost:8082/  >> cache_load_result.md
+echo " " >> cache_load_result.md
+echo "время 60 количество потоков 10 количество соединений 10"  >> cache_load_result.md
+wrk -d 60 -t 10 -c 10 --latency -s ./get.lua http://localhost:8082/  >> cache_load_result.md
+echo " " >> cache_load_result.md
+echo "время 60 количество потоков 50 количество соединений 50"  >> cache_load_result.md
+wrk -d 60 -t 50 -c 50 --latency -s ./get.lua http://localhost:8082/  >> cache_load_result.md
+echo " " >> cache_load_result.md
 
-> cache_load_result.md
